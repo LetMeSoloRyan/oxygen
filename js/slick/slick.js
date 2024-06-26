9087cf04e39db73a33b8d739338b14938fca02dd
LetMeSoloRyan <104810756+LetMeSoloRyan@users.noreply.github.com>
2024-06-27 02:19:24 +0800

    [Scripts] Moved slick initialization script into in an external JS file.

---
+++ js/slick/slick.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.carousel').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+});
